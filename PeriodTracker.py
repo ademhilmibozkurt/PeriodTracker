f163ef257226729086f75e65f3c88c13e10e02c7
ademhilmibozkurt <bozkurtadem7@gmail.com>
2024-12-20 09:43:04 +0300

    create_df function added, load_data and save_data functions filled

--- PeriodTracker.py
+++ PeriodTracker.py
@@ -1,10 +1,9 @@
 # Main program
 
-#import pandas as pd
 import time
 import calendar
+import pandas as pd
 
-TrackPoint = {'date':'', 'type':'', 'extra':''}
 InstanceType = {'Period Cramps Start':1, 'Period Cramps Persist':2, 'Period Cramps End':3, 'Ovulation Starts': 4, 'User Note':5}
 
 def Enter_Data ():
@@ -20,10 +19,10 @@ def Enter_Data ():
             match int(input()):
                 case 1:
                     # Do something
-                    return
+                    return 1
                 case 2:
                     # Do something
-                    return
+                    return 2
                 case 3:
                     # Do something
                     return
@@ -38,16 +37,32 @@ def Enter_Data ():
         except ValueError:
             invalidInput()
     instanceType = getInstanceType() # Select from options 1 - 5 for now, would like to make a gui for this eventually
-    
-    return
+    extra = input('Provide extra explanation.\n')
+
+    TrackPoint = {'date':date, 'type':instanceType, 'extra':extra}
+    return TrackPoint
+
+
+def create_df(df:pd.DataFrame, TrackPoint:dict):
+    return pd.DataFrame(data=TrackPoint).T
 
 # Loads the tracked data from a file
-def Load_Data ():
-    with open ('.\\PeriodData.csv', 'r+') as infile:
-        return
+def Load_Data (path:str):
+    try: 
+        with open (path, 'r') as infile:
+            return infile.read()
+    except FileNotFoundError:
+        print('File not found!')
 
 # Saves the tracked data to a file
-def Save_Data ():
-    with open ('.\\PeriodData.csv', 'w+') as outfile:
-        return
+def Save_Data (path:str, df:pd.DataFrame):
+    with open (path, 'a') as outfile:
+        outfile.write(df)
 
+def main():
+    trackPoint:dict = Enter_Data()
+    df = create_df(trackPoint) 
+    Save_Data('.\\PeriodData.csv', df)
+
+if '__name__' == '__main__':
+    main()
